892c1129671af16d9af766ec409e9e440ac69358
Nelson Gutierrez <nelson.gutierrez@swimlane.com>
2020-08-18 23:24:07 -0600

    Add firs components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardContainerComponent } from './components/card-container/card-container.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardContainerComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
