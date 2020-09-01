129462b6b39c4a2603e60fe8d1f032dc87baafc1
Nelson Gutierrez <nelson.gutierrez@swimlane.com>
2020-08-31 20:32:34 -0600

    Refactor to  flip-card module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,20 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CardContainerComponent } from './components/card-container/card-container.component';
 
-import { FlipCardComponent } from './components/flip-card/flip-card.component';
 import { AnswerCardComponent } from './components/answer-card/answer-card.component';
 import { QuestionCardComponent } from './components/question-card/question-card.component';
-import { FlipCardBackDirective } from './components/flip-card/flip-card-back.directive';
-import { FlipCardFrontDirective } from './components/flip-card/flip-card-front.directive';
+
+import { FlipCardModule } from './components/flip-card/flip-card.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CardContainerComponent,
-    FlipCardComponent,
     AnswerCardComponent,
     QuestionCardComponent,
-    FlipCardBackDirective,
-    FlipCardFrontDirective,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FlipCardModule],
   providers: [],
   bootstrap: [AppComponent],
 })
