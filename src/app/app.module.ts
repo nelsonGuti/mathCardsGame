53c8dd508a134982fa62e07c4eb632af862a8a2b
Nelson Gutierrez <nelson.gutierrez@swimlane.com>
2021-07-06 22:06:03 -0600

    Remove unnecesary import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
-import { FlipCardModule } from './components/home/flip-card/flip-card.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HomeModule } from './components/home/home.module';
 
