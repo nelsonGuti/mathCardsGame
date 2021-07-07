38a71f23d5403cab110118ea6e581229bd824088
Nelson Gutierrez <nelson.gutierrez@swimlane.com>
2021-07-06 22:04:38 -0600

    Update folder structure and added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { CardContainerComponent } from './components/card-container/card-container.component';
 
-import { AnswerCardComponent } from './components/answer-card/answer-card.component';
-import { QuestionCardComponent } from './components/question-card/question-card.component';
-
-import { FlipCardModule } from './components/flip-card/flip-card.module';
+import { AppRoutingModule } from './app-routing.module';
+import { FlipCardModule } from './components/home/flip-card/flip-card.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { OperatorsComponent } from './components/operators/operators.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { HomeModule } from './components/home/home.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    CardContainerComponent,
-    AnswerCardComponent,
-    QuestionCardComponent,
-    OperatorsComponent,
-    FooterComponent,
-  ],
-  imports: [BrowserModule, FlipCardModule, FontAwesomeModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HomeModule, FontAwesomeModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
