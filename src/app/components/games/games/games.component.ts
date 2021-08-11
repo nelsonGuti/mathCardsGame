6f6af403ee68b6195e42c1a41740ec388424c971
Nelson Gutierrez <nelson.gutierrez@swimlane.com>
2021-08-10 21:42:47 -0600

    Add settings and games pages

--- src/app/components/games/games/games.component.ts
+++ src/app/components/games/games/games.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faDivide,
+  faMinus,
+  faPlus,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons';
+import { Operation } from 'src/app/models/Operation';
+import { Operator } from 'src/app/models/Operator';
 
 @Component({
   selector: 'app-games',
   templateUrl: './games.component.html',
-  styleUrls: ['./games.component.scss']
+  styleUrls: ['./games.component.scss'],
 })
 export class GamesComponent implements OnInit {
+  operation: Operation = {
+    icon: faPlus,
+    name: Operator.ADDITION,
+    disabled: false,
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getSelectedOperation(operation): void {
+    this.operation = operation;
+    console.log('this.operation: ', this.operation);
   }
-
 }
